74b7f9d6c6f06c29c77ebdb88629d1fdb45470be
KateVarg <katye9470@gmail.com>
2024-05-13 20:24:19 +0200

    change test_homework.py

--- test_homework.py
+++ test_homework.py
@@ -9,7 +9,8 @@ def test_greeting():
     name = "Анна"
     age = 25
     # TODO Сформируйте нужную строку
-    output = "Привет, " + name + "! Тебе " + str(age) + " лет."
+    output = f"Привет, {name}! Тебе {age} лет."
+    print(output)
 
     # Проверяем результат
     assert output == "Привет, Анна! Тебе 25 лет."
@@ -24,11 +25,13 @@ def test_rectangle():
     b = 20
     # TODO сосчитайте периметр
     perimeter = (a + b) * 2
+    print(perimeter)
 
     assert perimeter == 60
 
     # TODO сосчитайте площадь
     area = a * b
+    print(area)
 
     assert area == 200
 
@@ -41,11 +44,13 @@ def test_circle():
     r = 23
     # TODO сосчитайте площадь
     area = math.pi * (r ** 2)
+    print(area)
 
     assert area == 1661.9025137490005
 
     # TODO сосчитайте длину окружности
     length = 2 * math.pi * r
+    print(length)
 
     assert length == 144.51326206513048
 
